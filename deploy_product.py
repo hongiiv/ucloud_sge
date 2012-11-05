9062c3246951bba80b7d5dd4d7bf0d6ed0a4499e
hongiiv <hongiiv@gmail.com>
2012-11-06 02:43:32 +0900

    %s 버그 수정

--- deploy_product.py
+++ deploy_product.py
@@ -228,7 +228,7 @@ EOF
 
 echo "deploy product from script" >> /tmp/deploy_result.txt
 echo "master ip: %s" >> /tmp/deploy_result.txt
-echo "slave ip: %s" >> /tmp/deploy_result.txt
+echo "master pw: %s" >> /tmp/deploy_result.txt
 
 /bin/rm /etc/init.d/userdata
 
@@ -250,13 +250,12 @@ cluster_uuid = %s
 
 log = logging.getLogger('bioinformatics')
 log.setLevel(logging.DEBUG)
-logging.basicConfig(format='[%(asctime)s] %(message)s')
+logging.basicConfig(format='[%%(asctime)s] %%(message)s')
 logging.warning('is when this event was logged.')
 
 def insert_message_instance():
    now = time.localtime()
-   queued_data = "%02d-%02d-%02d-%02d-%02d" % (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min)
-   autoscale_queue_instance_message = '{"clusteruuid":"%s"}'%(cluster_uuid)
+   autoscale_queue_instance_message = '{"clusteruuid":"%%s"}'%%(cluster_uuid)
    log.debug(autoscale_queue_instance_message)
    sqs_conn = boto.connect_sqs()
    autoscale_queue_instance = sqs_conn.create_queue(AUTOSCALE_QUEUE)
@@ -291,29 +290,28 @@ def main():
    for job_list in queue_info:
       job = {}
       job['id'] = job_list.find("JB_job_number").text
-      stime = datetime.datetime.strptime(job_list.find("JB_submission_time").text, "%Y-%m-%dT%H:%M:%S")
-      job['submitted'] = stime.strftime("%Y-%m-%d %H:%M:%S.%f")
-      re = "%s %s %s"%(job['id'], job['submitted'], datetime.datetime.today())
+      stime = datetime.datetime.strptime(job_list.find("JB_submission_time").text, "%%Y-%%m-%%dT%%H:%%M:%%S")
+      job['submitted'] = stime.strftime("%%Y-%%m-%%d %%H:%%M:%%S.%%f")
+      re = "%%s %%s %%s"%%(job['id'], job['submitted'], datetime.datetime.today())
       delta = datetime.datetime.now() - stime
       if delta.seconds > 120:
-         log.debug("autoscaling start: %s %d"%(delta.seconds, queue_size))
+         log.debug("autoscaling start: %%s %%d"%%(delta.seconds, queue_size))
          insert_message_instance()
          log.debug('Wait for new instance....')
          #20 min sleep for new instance
          for i in range(1200):
             time.sleep(1)
             percent = i/12
-            sys.stdout.write("\r%d%%" %percent)
+            sys.stdout.write("\\r%%d%%%%" %%percent)
             sys.stdout.flush()
       else:
-         log.debug("not yet: %s %d"%(delta.seconds, queue_size))
+         log.debug("not yet: %%s %%d"%%(delta.seconds, queue_size))
 
 if __name__ == "__main__":
     while True:
         main()
         time.sleep(2)
 EOF
-
    '''%(sge_config_file, sge_config_file, sge_host_file, sge_host_file, sge_host_file, sge_config_file, sge_config_file, productid, productid, master_private_address, master_password, productid, master_private_address, master_password, productid)
 
    script_name = ("%s_master.sh")%(productid)
